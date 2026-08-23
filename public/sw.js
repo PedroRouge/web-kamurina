e928fb9cef881826062af26d388daa3c5c265888
Pedvisuals_ <pedrorouge2022@gmail.com>
2026-08-22 23:36:36 -0300

    version 0.7 after stable

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,13 @@
-const CACHE_NAME = 'atelier-app-v1';
+const CACHE_NAME = 'atelier-app-v2';
 
-self.addEventListener('install', () => {
-  self.skipWaiting();
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll([
+      '/',
+      '/index.html',
+      '/manifest.json'
+    ])).then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', (event) => {
