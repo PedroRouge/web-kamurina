d0132dd0bd80931a9d0102d1ead481d678abf699
Pedvisuals_ <pedrorouge2022@gmail.com>
2026-08-22 23:38:09 -0300

    version 0.8 after stable

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,38 @@
-const CACHE_NAME = 'atelier-app-v2';
+const CACHE_NAME = 'atelier-app-v3';
+
+const cacheAppShell = async () => {
+  const cache = await caches.open(CACHE_NAME);
+  const indexResponse = await fetch('/index.html');
+  await cache.put('/index.html', indexResponse.clone());
+  await cache.put('/', indexResponse.clone());
+
+  const pendingUrls = ['/manifest.json'];
+  const html = await indexResponse.text();
+  const htmlAssets = [...html.matchAll(/(?:src|href)=["']([^"']+)["']/g)].map((match) => match[1]);
+  pendingUrls.push(...htmlAssets);
+
+  const cachedUrls = new Set();
+  while (pendingUrls.length > 0) {
+    const assetUrl = pendingUrls.shift();
+    const absoluteUrl = new URL(assetUrl, self.location.origin);
+    if (absoluteUrl.origin !== self.location.origin || cachedUrls.has(absoluteUrl.href)) continue;
+    cachedUrls.add(absoluteUrl.href);
+
+    const response = await fetch(absoluteUrl.href);
+    if (!response.ok) continue;
+    await cache.put(absoluteUrl.href, response.clone());
+
+    if (absoluteUrl.pathname.endsWith('.js')) {
+      const script = await response.text();
+      const moduleAssets = [...script.matchAll(/import\(["']([^"']+)["']\)/g)].map((match) => match[1]);
+      pendingUrls.push(...moduleAssets.map((moduleUrl) => new URL(moduleUrl, absoluteUrl).href));
+    }
+  }
+};
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll([
-      '/',
-      '/index.html',
-      '/manifest.json'
-    ])).then(() => self.skipWaiting())
+    cacheAppShell().then(() => self.skipWaiting())
   );
 });
 
